0757e7c720da4b2828735947817c68d51682a2b7
Iordan Ionut <mariusionut879@yahoo.com>
2024-02-13 18:50:52 +0200

    Initial commit

--- src/main/java/com/example/DataBase/Primary/asda.java
+++ src/main/java/com/example/DataBase/Primary/asda.java
@@ -1,4 +1,8 @@
 package com.example.DataBase.Primary;
 
+import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails;
+
 public class asda {
+    private String A;
+    RabbitConnectionDetails.Address a;
 }
